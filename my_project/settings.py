f156639cb2b4b3c102ae568f8892e740febd5d2d
Terry Martin <terry.martin@gmail.com>
2024-10-12 13:50:47 +0000

    Connect to heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+da5sd!@&++2s^d9r0n+owjeyzcbg=qsdd9(@-ho)^zamn3hq6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-terrymartin-djangoproje-vz2xnbwo5k8.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-terrymartin-djangoproje-vz2xnbwo5k8.ws.codeinstitute-ide.net',
+                '.herokuapp.com']
 # ALLOWED_HOSTS = ['http://8000-terrymartin-djangoproje-vz2xnbwo5k8.ws.codeinstitute-ide.net/']
 
 
